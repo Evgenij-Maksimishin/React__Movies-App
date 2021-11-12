e265951f38672496f9a0ce85b63f7579d244b4b0
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-13 00:50:58 +0200

    createfn handleClick

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,23 +1,38 @@
+import { values } from "lodash";
 import React from "react";
 
 
 const MovieTabs = (props) => {
-    const { sort_by } = props;
+    const { sort_by, updateSortBy } = props;
+    const handleClick = (value) => {
+        return () => {
+            updateSortBy(value)
+        }
+    }
     return (
         <div>
             <ul className="tabs nav nav-pills">
                 <li className="nav-item">
-                    <div className={`nav-link ${sort_by === "popularity.desc" ? "active" : ""}`}>
+                    <div
+                        className={`nav-link ${sort_by === "popularity.desc" ? "active" : ""}`}
+                        onClick={handleClick("popularity.desc")}
+                    >
                         Popularity
                     </div>
                 </li>
                 <li className="nav-item">
-                    <div className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}>
+                    <div
+                        className={`nav-link ${sort_by === "revenue.desc" ? "active" : ""}`}
+                        onClick={handleClick("revenue.desc")}
+                    >
                         Revenue
                     </div>
                 </li>
                 <li className="nav-item">
-                    <div className={`nav-link ${sort_by === "vote_avarage.desc" ? "active" : ""}`}>
+                    <div
+                        className={`nav-link ${sort_by === "vote_average.desc" ? "active" : ""}`}
+                        onClick={handleClick("vote_average.desc")}
+                    >
                         Vote average desc
                     </div>
                 </li>
