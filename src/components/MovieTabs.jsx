252f37913e64bf3080b78419beeaabfc3c0091b1
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-15 00:24:46 +0200

    create shouldComponentUpdate

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,48 +1,59 @@
 import { values } from "lodash";
 import React from "react";
 
+class MovieTabs extends React.Component {
 
-const MovieTabs = (props) => {
-    const { sort_by, updateSortBy } = props;
-    const handleClick = (value) => {
-        return () => {
-            updateSortBy(value)
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.sort_by !== this.state.sort_by){
+            return true
+        } else {
+            return false
         }
     }
-    const getClassLink = (value) => {
-        return `nav-link ${sort_by === value ? "active" : ""}`
-    }
-    return (
-        <div>
-            <ul className="tabs nav nav-pills">
-                <li className="nav-item">
-                    <div
-                        className={getClassLink("popularity.desc")}
-                        onClick={handleClick("popularity.desc")}
-                    >
-                        Popularity
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <div
-                        className={getClassLink("revenue.desc")}
-                        onClick={handleClick("revenue.desc")}
-                    >
-                        Revenue
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <div
-                        className={getClassLink("vote_average.desc")}
-                        onClick={handleClick("vote_average.desc")}
-                    >
-                        Vote average desc
-                    </div>
-                </li>
-            </ul>
 
-        </div>
-    )
+    render() {
+        const { sort_by, updateSortBy } = this.props;
+        const handleClick = (value) => {
+            return () => {
+                updateSortBy(value)
+            }
+        }
+        const getClassLink = (value) => {
+            return `nav-link ${sort_by === value ? "active" : ""}`
+        }
+
+        return (
+            <div>
+                <ul className="tabs nav nav-pills">
+                    <li className="nav-item">
+                        <div
+                            className={getClassLink("popularity.desc")}
+                            onClick={handleClick("popularity.desc")}
+                        >
+                            Popularity
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <div
+                            className={getClassLink("revenue.desc")}
+                            onClick={handleClick("revenue.desc")}
+                        >
+                            Revenue
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <div
+                            className={getClassLink("vote_average.desc")}
+                            onClick={handleClick("vote_average.desc")}
+                        >
+                            Vote average desc
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        )
+    }
 }
 
 export default MovieTabs;
